0f52731cc2c53c61b895821636b02b95b1607696
paulb <paulb@ashridgetech.com>
2016-07-18 15:09:34 +0100

    Routing with PersistentRouter that does not destroy elements while they are hidden

--- main/view/App.js
+++ main/view/App.js
@@ -1,10 +1,16 @@
 const React = require('react')
+const PersistentRouter = require('./PersistentRouter')
 const VisibleAccountList = require('./VisibleAccountList')
+const MainPage = require('./MainPage')
+const {Locations, Location} = require('react-router-component')
 
 const App = () => (
     <div>
-        <VisibleAccountList />
-    </div>
-)
+        <h1>Books</h1>
+        <PersistentRouter hash>
+            <Location path="/" handler={MainPage}/>
+            <Location path="/account" handler={VisibleAccountList}/>
+        </PersistentRouter>
+    </div>)
 
 module.exports = App
---
+++ main/view/MainPage.js
@@ -0,0 +1,9 @@
+const React = require('react')
+
+const MainPage = () => (
+    <div>
+        <h2>Welcome to ReactBooks</h2>
+    </div>
+)
+
+module.exports = MainPage
