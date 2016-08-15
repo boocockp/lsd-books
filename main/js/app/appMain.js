f0ff9d188f87855eef9abfe6e2dc2cfcb3f65195
paulb <paulb@ashridgetech.com>
2016-08-15 12:20:41 +0100

    Removed auth dir

--- main/js/app/appMain.js
+++ main/js/app/appMain.js
@@ -11,8 +11,8 @@ const SynchronizingStore = require('../store/SynchronizingStore')
 const PersistentStore = require('../store/PersistentStore')
 const App = require('../view/App')
 const AppProvider = require('../view/AppProvider')
-const GoogleSigninTracker = require('../auth/GoogleSigninTracker')
-const CognitoCredentialsSource = require('../auth/CognitoCredentialsSource')
+const GoogleSignin = require('superviews').GoogleSignin
+const CognitoCredentialsSource = require('../store/CognitoCredentialsSource')
 
 const config = {
     "clientId": "919408445147-a0csgn7e21d773ilrif3q8d9hfrfc7vm.apps.googleusercontent.com",
@@ -31,7 +31,7 @@ const reduxStore = createStore(booksReducer, new Books());
 const appStore = new SynchronizingStore(reduxStore)
 
 const localStore = new LocalStorageUpdateStore(appConfig.appName, appConfig.dataSet)
-const googleSigninTracker = new GoogleSigninTracker()
+const googleSigninTracker = new GoogleSignin.Tracker()
 const cognitoCredentialsSource = new CognitoCredentialsSource(config.identityPoolId)
 googleSigninTracker.signIn.sendTo(cognitoCredentialsSource.signIn)
 const remoteStore = new S3UpdateStore(config.bucketName, 'updates', appConfig.appName, appConfig.dataSet, cognitoCredentialsSource)
