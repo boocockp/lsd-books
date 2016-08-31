1e0508909a757e45dc7c68a34012431e08b4abbd
paulb <paulb@ashridgetech.com>
2016-08-31 18:01:41 +0100

    Build using separate webpack config

---
+++ webpack.lambda.config.js
@@ -0,0 +1,38 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+var webpack = require("webpack")
+var path = require('path')
+
+var dir_js = path.resolve(__dirname, 'lambda');
+var dir_build = path.resolve(__dirname, 'build_lambda');
+
+module.exports = {
+    entry: {
+        promoter: path.resolve(dir_js, 'promoter/index.js'),
+    },
+    output: {
+        path: path.resolve(dir_build, 'promoter'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    externals: {
+        "aws-sdk": true
+    },
+    module: {
+        noParse: [
+            /aws\-sdk/,
+        ],
+        loaders: [
+            {
+                loader: 'babel-loader?cacheDirectory',
+                test: [/\.js$/],
+                exclude: [
+                    path.resolve(__dirname, "node_modules"),
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // new webpack.PrefetchPlugin('./node_modules/react-bootstrap/lib/PageItem.js'),
+        // new webpack.optimize.CommonsChunkPlugin({ names: ['vendor', 'manifest']}),
+    ],
+};
